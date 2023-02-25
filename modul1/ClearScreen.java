daea72ab34fa200618609b3590eae325622e6161
Jordi Irawan <jordirwn@gmail.com>
2023-02-26 06:36:43 +0700

    ✨ feat: sampel test

--- modul1/ClearScreen.java
+++ modul1/ClearScreen.java
@@ -2,6 +2,15 @@ package modul1;
 
 public class ClearScreen {
     public static void main(String[] args) {
+        // sampel output
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
+
+        clearScreen();
+    }
+
+    public static void clearScreen() {
         // \033[H : memindahkan cursor ke kiri atas konsole
         // \033[2J : menghapus output di layar
         System.out.print("\033[H\033[2J");
